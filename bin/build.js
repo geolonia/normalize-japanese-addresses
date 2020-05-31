a304baed8c73f10cd2a4627e2be0350dcc92371b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-05-31 17:39:07 +0900

    some fix

--- bin/build.js
+++ bin/build.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 const path = require("path")
 const unzip= require('unzipper')
 const Encoding = require('encoding-japanese')
-const http = require('http')
+const https = require('https')
 const parse = require('csv-parse/lib/sync')
 
 const apiDir = path.join(path.dirname(path.dirname(__filename)), 'docs', 'api')
@@ -17,56 +17,55 @@ for (let i = 1; i < 48; i++) {
     prefCode = `0${prefCode}`
   }
 
-  const url = `http://nlftp.mlit.go.jp/isj/dls/data/11.0b/${prefCode}000-11.0b.zip`
-  const file = fs.createWriteStream(`${dataDir}/${prefCode}.zip`)
+  const url = `https://nlftp.mlit.go.jp/isj/dls/data/11.0b/${prefCode}000-11.0b.zip`
+  const fileName = `${dataDir}/${prefCode}.zip`
+  const file = fs.createWriteStream(fileName)
 
-  var req = http.get(url, (res) => {
+  https.get(url, (res) => {
     res.pipe(file);
     res.on('end', () => {
-        file.close();
-    });
-  });
+      file.close();
+
+      const dir = path.join(dataDir, `${prefCode}000-11.0b`)
+      fs.createReadStream(fileName).pipe(unzip.Extract({ path: dataDir }))
+      const csvFile = `${dir}/${prefCode}_2017.csv`
+
+      setTimeout(() => {
+        const buffer = fs.readFileSync(csvFile)
+        const text = Encoding.convert(buffer, {
+          from: 'SJIS',
+          to: 'UNICODE',
+          type: 'string',
+        })
+
+        const data = parse(text, {
+          columns: true,
+          skip_empty_lines: true
+        })
+
+        data.forEach((line) => {
+          const dir = path.join(apiDir, line['都道府県コード'], line['市区町村コード'])
+          const json = JSON.stringify({
+            addr: `${line['都道府県名']}${line['市区町村名']}${line['大字町丁目名']}`,
+            lat: line['緯度'],
+            lng: line['経度'],
+          })
+
+          try {
+            const stats = fs.statSync(dir)
+            if (stats.isDirectory()) {
+              fs.writeFileSync(path.join(dir, `${line['大字町丁目コード']}.json`), json)
+            } else {
+              throw new Error()
+            }
+          } catch(e) {
+            fs.mkdirSync(dir, {recursive: true})
+            // This is the default endpoint for the city.
+            fs.writeFileSync(path.join(dir, `${line['市区町村コード']}.json`), json)
+            fs.writeFileSync(path.join(dir, `${line['大字町丁目コード']}.json`), json)
+          }
+        })
+      }, 2000)
+    })
+  })
 }
-
-
-// const files = glob.sync(`${dataDir}/*-11.0b.zip`)
-
-// files.forEach((item) => {
-//   const dir = path.join(dataDir, path.basename(item, '.zip'))
-//   fs.createReadStream(item).pipe(unzip.Extract({ path: dataDir }))
-//   const csvFile = glob.sync(`${dir}/*.csv`)[0]
-//   const buffer = fs.readFileSync(csvFile)
-//   const text = Encoding.convert(buffer, {
-//     from: 'SJIS',
-//     to: 'UNICODE',
-//     type: 'string',
-//   })
-
-//   const data = parse(text, {
-//     columns: true,
-//     skip_empty_lines: true
-//   })
-
-//   data.forEach((line) => {
-//     const dir = path.join(apiDir, line['都道府県コード'], line['市区町村コード'])
-//     const json = JSON.stringify({
-//       addr: `${line['都道府県名']}${line['市区町村名']}${line['大字町丁目名']}`,
-//       lat: line['緯度'],
-//       lng: line['経度'],
-//     })
-
-//     try {
-//       const stats = fs.statSync(dir)
-//       if (stats.isDirectory()) {
-//         fs.writeFileSync(path.join(dir, `${line['大字町丁目コード']}.json`), json)
-//       } else {
-//         throw new Error()
-//       }
-//     } catch(e) {
-//       fs.mkdirSync(dir, {recursive: true})
-//       // This is the default endpoint for the city.
-//       fs.writeFileSync(path.join(dir, `${line['市区町村コード']}.json`), json)
-//       fs.writeFileSync(path.join(dir, `${line['大字町丁目コード']}.json`), json)
-//     }
-//   })
-// })
