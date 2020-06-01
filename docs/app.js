356767b69e0758c054858c0144af62243de4734f
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-02 05:37:04 +0900

    some fix

--- docs/app.js
+++ docs/app.js
@@ -19,14 +19,29 @@ document.addEventListener('DOMContentLoaded', () => {
     e.target.select()
   })
 
+  const showMessage = (msg) => {
+    document.querySelector('#err .msg').textContent = msg
+    document.getElementById('err').style.visibility = 'visible'
+
+    const title = `[誤判定] ${document.getElementById('address').value}`
+    const body = `住所が判定できませんでした。`
+    const url = `https://github.com/geolonia/community-geocoder/issues/new?title=${encodeURI(title)}&body=${encodeURI(body)}`
+    document.querySelector('#err a').href = url
+  }
+
   const search = () => {
-    document.getElementById('err').textContent = ''
+    document.getElementById('err').style.visibility = 'hidden'
+
     if (document.getElementById('address').value) {
       getLatLng(document.getElementById('address').value, (latlng) => {
         console.log(latlng)
+        if (5 === latlng.code.length) {
+          showMessage(`住所の判定ができなかったので「${latlng.addr}」に移動します。`)
+        }
         map.flyTo({center: latlng, zoom: 16, essential: true})
       }, (e) => {
-        document.getElementById('err').textContent = e.message
+        document.querySelector('#err .msg').textContent = e.message
+        showMessage(e.message)
       })
     }
   }
