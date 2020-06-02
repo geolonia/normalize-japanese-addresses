8a897ea1aa0dec62dfd038b7982ac40995a35e28
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-03 01:14:27 +0900

    added tests for normalize

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,8 +1,36 @@
-const chai = require('chai');
+import chai from 'chai';
 const assert = chai.assert;
 
+import util from '../src/imi-enrichment-address/lib/util'
+
 describe('Tests for `src/imi-enrichment-address/lib/util.js`.', () => {
-  it('Get correct URL of Geolonia\'s style.', () => {
-    assert.deepEqual(1, 0);
+  it('should normalize address "大阪府堺市北区新金岡町4丁1−8".', () => {
+    const normalized = util.normalize("大阪府堺市北区新金岡町4丁1−8")
+    assert.deepEqual("大阪府堺市北区新金岡町四丁1−8", normalized)
+  });
+
+  it('should normalize address "大阪府堺市北区新金岡町26丁1−8".', () => {
+    const normalized = util.normalize("大阪府堺市北区新金岡町26丁1−8")
+    assert.deepEqual("大阪府堺市北区新金岡町二十六丁1−8", normalized)
+  });
+
+  it('should normalize address "大阪府堺市北区新金岡町２６丁１−８".', () => {
+    const normalized = util.normalize("大阪府堺市北区新金岡町26丁１−８")
+    assert.deepEqual("大阪府堺市北区新金岡町二十六丁１−８", normalized)
+  });
+
+  it('should normalize address "札幌市中央区北1条西2丁目".', () => {
+    const normalized = util.normalize("札幌市中央区北1条西2丁目")
+    assert.deepEqual("札幌市中央区北一条西2丁目", normalized)
+  });
+
+  it('should normalize address "札幌市中央区北２１条西2丁目".', () => {
+    const normalized = util.normalize("札幌市中央区北２１条西2丁目")
+    assert.deepEqual("札幌市中央区北二十一条西2丁目", normalized)
+  });
+
+  it('should normalize address "大阪府大阪市北区梅田１丁目２".', () => {
+    const normalized = util.normalize("大阪府大阪市北区梅田１丁目２")
+    assert.deepEqual("大阪府大阪市北区梅田１丁目２", normalized)
   });
 })
