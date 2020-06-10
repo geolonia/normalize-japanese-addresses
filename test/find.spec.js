dad092a58bcb1ecb0d28daca68cda594dca2abdc
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-10 11:25:52 +0900

    added tests for kyoto

--- test/find.spec.js
+++ test/find.spec.js
@@ -1,5 +1,7 @@
 import chai from 'chai';
 const assert = chai.assert;
+import fs from 'fs'
+import path from 'path'
 
 import util from '../src/imi-enrichment-address/lib/util'
 import find from '../src/imi-enrichment-address/lib/find'
@@ -229,4 +231,16 @@ describe('Tests for `src/imi-enrichment-address/lib/find.js`.', () => {
     assert.deepEqual('262040030000', res.code)
     assert.deepEqual('二丁目', res.tail)
   });
+})
+
+describe('Tests for `src/imi-enrichment-address/lib/find.js` with address list.', () => {
+  const data = fs.readFileSync(path.join(path.dirname(__filename), '/kyoto.txt'), {encoding: 'utf-8'}).split(/\n/)
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]) {
+      it(`should find the address "${data[i]}" as expected.`, () => {
+        const res = find(util.normalize(data[i]))
+        assert.deepEqual(12, res.code.length)
+      });
+    }
+  }
 })
