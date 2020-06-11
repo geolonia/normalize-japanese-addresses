adab1a48d52b7f9af06de6b6668747bf77cc28dd
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-12 02:18:54 +0900

    bug fix

--- src/app.js
+++ src/app.js
@@ -1,32 +1,32 @@
 import geojsonExtent from '@mapbox/geojson-extent'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const map = new geolonia.Map('#map')
+  const map = new window.geolonia.Map('#map')
 
   map.on('moveend', () => {
-    const {lng, lat} = map.getCenter()
+    const { lng, lat } = map.getCenter()
     document.getElementById('lat').value = Math.round(lat * 1000000) / 1000000
     document.getElementById('lng').value = Math.round(lng * 1000000) / 1000000
   })
 
-  document.getElementById('address').addEventListener('focus', (e) => {
+  document.getElementById('address').addEventListener('focus', e => {
     e.target.select()
   })
 
-  document.getElementById('lat').addEventListener('focus', (e) => {
+  document.getElementById('lat').addEventListener('focus', e => {
     e.target.select()
   })
 
-  document.getElementById('lng').addEventListener('focus', (e) => {
+  document.getElementById('lng').addEventListener('focus', e => {
     e.target.select()
   })
 
-  const showMessage = (msg) => {
+  const showMessage = msg => {
     document.querySelector('#err .msg').textContent = msg
     document.getElementById('err').style.visibility = 'visible'
 
     const title = `[誤判定] ${document.getElementById('address').value}`
-    const body = `住所が判定できませんでした。`
+    const body = '住所が判定できませんでした。'
     const url = `https://github.com/geolonia/community-geocoder/issues/new?title=${encodeURI(title)}&body=${encodeURI(body)}`
     document.querySelector('#err a').href = url
   }
@@ -40,14 +40,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('err').style.visibility = 'hidden'
 
     if (document.getElementById('address').value) {
-      getLatLng(document.getElementById('address').value, (latlng) => {
+      window.getLatLng(document.getElementById('address').value, latlng => {
+        // eslint-disable-next-line no-console
         console.log(latlng)
         if (5 === latlng.code.length) {
           const endpoint = `https://geolonia.github.io/japanese-admins/${latlng.code.substr(0, 2)}/${latlng.code}.json`
-          console.log(endpoint)
-          fetch(endpoint).then((res) => {
+          fetch(endpoint).then(res => {
             return res.json()
-          }).then((data) => {
+          }).then(data => {
             map.fitBounds(geojsonExtent(data))
             map.addLayer({
               id: 'japanese-administration',
@@ -56,18 +56,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 type: 'geojson',
                 data: data,
               },
-              'layout': {},
-              'paint': {
+              layout: {},
+              paint: {
                 'fill-color': '#ff0000',
-                'fill-opacity': 0.05
-              }
+                'fill-opacity': 0.05,
+              },
             })
             showMessage(`住所の判定ができなかったので「${data.features[0].properties.name}」に移動します。`)
           })
         } else {
-          map.flyTo({center: latlng, zoom: 16, essential: true})
+          map.flyTo({ center: latlng, zoom: 16, essential: true })
         }
-      }, (e) => {
+      }, e => {
         document.querySelector('#err .msg').textContent = e.message
         showMessage(e)
       })
