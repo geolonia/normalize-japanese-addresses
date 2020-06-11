c8c0e0b8670317ed08d457ccbe7be768c6962d23
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-11 18:17:20 +0900

    added function to convert kanji

--- test/find.spec.js
+++ test/find.spec.js
@@ -1,7 +1,5 @@
 import chai from 'chai';
 const assert = chai.assert;
-import fs from 'fs'
-import path from 'path'
 
 import util from '../src/imi-enrichment-address/lib/util'
 import find from '../src/imi-enrichment-address/lib/find'
@@ -295,15 +293,3 @@ describe('Tests for `src/imi-enrichment-address/lib/find.js`.', () => {
     assert.deepEqual('', res.tail) // 内部的に `之` を `の` に変換
   })
 })
-
-// describe('Tests for `src/imi-enrichment-address/lib/find.js` with address list.', () => {
-//   const data = fs.readFileSync(path.join(path.dirname(__filename), '/addresses.txt'), {encoding: 'utf-8'}).split(/\n/)
-//   for (let i = 0; i < data.length; i++) {
-//     if (data[i]) {
-//       it(`should find the address "${data[i]}" as expected.`, () => {
-//         const res = find(util.normalize(data[i]))
-//         assert.isTrue(12 == res.code.length)
-//       });
-//     }
-//   }
-// })
