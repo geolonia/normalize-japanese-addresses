41381a4a12e2bad8412b9e968718864563ae7f10
kamataryo <kamataryo@users.noreply.github.com>
2020-09-29 18:22:37 +0900

    Fix build

--- src/lib/bangou.ts
+++ src/lib/bangou.ts
@@ -1,4 +1,4 @@
-import util from './util'
+import { a2h } from './util'
 
 export default (src: string) => {
   const dst: Geolonia.NormalizeJapaneseAddresses.Bangou = {}
@@ -8,7 +8,7 @@ export default (src: string) => {
       /^([0-9０-９一二三四五六七八九十〇]+)(番地|番)([0-9０-９一二三四五六七八九十〇]+)号/,
     )
   ) {
-    const v = [RegExp.$1, RegExp.$3].map((x) => util.a2h(x))
+    const v = [RegExp.$1, RegExp.$3].map((x) => a2h(x))
     dst['番地'] = v[0]
     dst['号'] = v[1]
   } else if (
@@ -16,14 +16,14 @@ export default (src: string) => {
       /^([0-9０-９一二三四五六七八九十〇]+)(ー|－|-)([0-9０-９一二三四五六七八九十〇]+)/,
     )
   ) {
-    const v = [RegExp.$1, RegExp.$3].map((x) => util.a2h(x))
+    const v = [RegExp.$1, RegExp.$3].map((x) => a2h(x))
     dst['番地'] = v[0]
     dst['号'] = v[1]
   } else if (src.match(/^([0-9０-９一二三四五六七八九十〇]+)(番地|番)/)) {
-    const v = [RegExp.$1].map((x) => util.a2h(x))
+    const v = [RegExp.$1].map((x) => a2h(x))
     dst['番地'] = v[0]
   } else if (src.match(/^([0-9０-９一二三四五六七八九十〇]+)/)) {
-    const v = [RegExp.$1].map((x) => util.a2h(x))
+    const v = [RegExp.$1].map((x) => a2h(x))
     dst['番地'] = v[0]
   }
   return dst
