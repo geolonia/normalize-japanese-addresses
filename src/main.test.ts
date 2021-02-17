810501e4d6a5343d96c113ee4630ce9068ebac9c
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-02-18 04:48:35 +0900

    normalize town

--- src/main.test.ts
+++ src/main.test.ts
@@ -15,3 +15,13 @@ test('和歌山県東牟婁郡串本町串本1234', async () => {
   expect(res).toBe('和歌山県東牟婁郡串本町串本1234');
 });
 
+test('和歌山県東牟婁郡串本町串本一二三四', async () => {
+  const res = await normalize('和歌山県東牟婁郡串本町串本一二三四')
+  expect(res).toBe('和歌山県東牟婁郡串本町串本1234');
+});
+
+test('京都府京都市中京区寺町通御池上る上本能寺前町488番地', async () => {
+  const res = await normalize('京都府京都市中京区寺町通御池上る上本能寺前町488番地')
+  expect(res).toBe('京都府京都市中京区上本能寺前町488番地');
+});
+
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
-import { kanji2number } from '@geolonia/japanese-numeral'
-import fetch from 'node-fetch';
+import os from 'os'
+import path from 'path'
+import { kanji2number, findKanjiNumbers } from '@geolonia/japanese-numeral'
+const tmpdir = path.join(os.tmpdir(), 'normalize-japanese-addresses')
+const fetch = require('node-fetch-cache')(tmpdir);
 import dict from './lib/dict'
 
 const endpoint = 'https://cdn.geolonia.com/address/japan'
 
+const kan2num = (string: string) => {
+  const kanjiNumbers = findKanjiNumbers(string)
+  for (let i = 0; i < kanjiNumbers.length; i++) {
+    // @ts-ignore
+    string = string.replace(kanjiNumbers[i], kanji2number(kanjiNumbers[i]))
+  }
+
+  return string
+}
+
+const zen2han = (str: string) => {
+  return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+  });
+}
+
 export const normalize = async (address: string) => {
-  let addr = dict(address)
+  let addr = dict(zen2han(address))
+
+  // 都道府県名の正規化
 
   const responsePrefs = await fetch(`${endpoint}.json`);
   const prefs = await responsePrefs.json();
@@ -18,6 +39,8 @@ export const normalize = async (address: string) => {
       break
     }
   }
+
+  // 市区町村名の正規化
 
   const responseCities = await fetch(`${endpoint}/${encodeURI(pref)}.json`);
   const cities = await responseCities.json();
@@ -41,8 +64,26 @@ export const normalize = async (address: string) => {
     }
   }
 
-  // 以降町丁目以降の正規化
+  // 町丁目以降の正規化
 
+  const responseTowns = await fetch(`${endpoint}/${encodeURI(pref)}/${encodeURI(city.replace(/.+郡/, ''))}.json`);
+  const towns = await responseTowns.json();
 
-  return pref + city + addr
+  addr = kan2num(addr) // 漢数字を数字に
+
+  let town = ''
+  for (let i = 0; i < towns.length; i++) {
+    const _town = kan2num(dict(towns[i]))
+    if (0 === addr.indexOf(_town)) {
+      town = kan2num(towns[i])
+      addr = addr.substr(kan2num(towns[i]).length) // 町丁目以降の住所
+      break
+    } else if ('京都府' === pref && 0 <= addr.lastIndexOf(_town)) {
+      town = kan2num(towns[i])
+      const reg = new RegExp(`^.*${_town}`)
+      addr = addr.replace(reg, '') // 町丁目以降の住所
+    }
+  }
+
+  return pref + city + town + addr
 }
