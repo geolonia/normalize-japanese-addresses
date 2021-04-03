b8c9f3118f9e9045f629af919ebcfb20e62f20ae
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-04-03 22:03:44 +0900

    added test

--- test/addresses.test.ts
+++ test/addresses.test.ts
@@ -3,19 +3,21 @@ import { normalize } from '../src/main'
 import fs from 'fs'
 import path from 'path'
 
-const data = fs.readFileSync(path.join(path.dirname(__filename), '/list1.csv'), {encoding: 'utf-8'}).split(/\n/)
+const lines = fs.readFileSync(path.join(path.dirname(__filename), '/addresses.csv'), {encoding: 'utf-8'}).split(/\n/)
+lines.shift() // 見出し行
 
-for (let i = 0; i < data.length; i++) {
-  const address = data[i].trim()
-  if (! address) {
+for (let i = 0; i < lines.length; i++) {
+  if (! lines[i]) {
     continue
   }
 
-  test(address, async () => {
-    const res = await normalize(address)
-    expect(!! res.pref).toStrictEqual(true)
-    expect(!! res.city).toStrictEqual(true)
-    expect(!! res.town).toStrictEqual(true)
-    expect(!! res.addr).toStrictEqual(true)
+  const data = lines[i].trim().split(/,/)
+
+  test(data[0], async () => {
+    const res = await normalize(data[0])
+    expect(res.pref).toStrictEqual(data[1])
+    expect(res.city).toStrictEqual(data[2])
+    expect(res.town).toStrictEqual(data[3])
+    expect(res.addr).toStrictEqual(data[4])
   })
 }
