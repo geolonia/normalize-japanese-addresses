0dfcd765da7a82302dc8ae9816f464454d34dcb1
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-04-03 21:41:50 +0900

    some fix

--- test/build-test-data.ts
+++ test/build-test-data.ts
@@ -2,6 +2,8 @@ import { normalize } from '../src/main'
 import fs from 'fs'
 import path from 'path'
 
+console.log('住所,都道府県,市区町村,町丁目,その他')
+
 const data = fs.readFileSync(path.join(path.dirname(__filename), '/list.txt'), {encoding: 'utf-8'}).split(/\n/)
 
 const lines = []
