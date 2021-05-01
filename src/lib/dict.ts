fc7e9d5f8f98ebc2a2d8a0513b9a5580c139f091
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-05-01 17:45:13 +0900

    added example and improve regex order

--- src/lib/dict.ts
+++ src/lib/dict.ts
@@ -27,21 +27,22 @@ export const toRegex = (string: string) => {
 
   // 以下なるべく文字数が多いものほど上にすること
   _str = _str
-    .replace(/塚|塚/g, '(塚|塚)')
     .replace(/三栄町|四谷三栄町/g, '(三栄町|四谷三栄町)')
+    .replace(/鬮野川|くじ野川|くじの川/g, '(鬮野川|くじ野川|くじの川)')
     .replace(/通り|とおり/g, '(通り|とおり)')
     .replace(/埠頭|ふ頭/g, '(埠頭|ふ頭)')
-    .replace(/鬮野川|くじ野川|くじの川/g, '(鬮野川|くじ野川|くじの川)')
+    .replace(/番町|番丁/g, '(番町|番丁)')
+    .replace(/大冝|大宜/g, '(大冝|大宜)')
+    .replace(/穝|さい/g, '(穝|さい)')
+    .replace(/杁|えぶり/g, '(杁|えぶり)')
+    .replace(/薭|稗|ひえ|ヒエ/g, '(薭|稗|ひえ|ヒエ)')
     .replace(/[之ノの]/g, '[之ノの]')
     .replace(/[ヶケが]/g, '[ヶケが]')
     .replace(/[ヵカか力]/g, '[ヵカか力]')
     .replace(/[ッツっつ]/g, '[ッツっつ]')
     .replace(/[ニ二]/g, '[ニ二]')
     .replace(/[ハ八]/g, '[ハ八]')
-    .replace(/大冝|大宜/g, '(大冝|大宜)')
-    .replace(/穝|さい/g, '(穝|さい)')
-    .replace(/杁|えぶり/g, '(杁|えぶり)')
-    .replace(/薭|稗|ひえ|ヒエ/g, '(薭|稗|ひえ|ヒエ)')
+    .replace(/塚|塚/g, '(塚|塚)')
     .replace(/釜|竈/g, '(釜|竈)')
     .replace(/條|条/g, '(條|条)')
     .replace(/狛|拍/g, '(狛|拍)')
@@ -49,7 +50,6 @@ export const toRegex = (string: string) => {
     .replace(/渕|淵/g, '(渕|淵)')
     .replace(/エ|ヱ|え/g, '(エ|ヱ|え)')
     .replace(/曾|曽/g, '(曾|曽)')
-    .replace(/番町|番丁/g, '(番町|番丁)')
 
   _str = jisKanji(_str)
 
