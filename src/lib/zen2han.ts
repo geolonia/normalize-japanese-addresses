f2282ead42c187eba6ef5f9db1246209c2014777
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-05-01 21:23:25 +0900

    Removed meaningless condition.

--- src/lib/zen2han.ts
+++ src/lib/zen2han.ts
@@ -1,9 +1,5 @@
 export const zen2han = (str: string) => {
   return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
-    if ('ー' === s || '−' === s) {
-      return '-'
-    } else {
-      return String.fromCharCode(s.charCodeAt(0) - 0xfee0)
-    }
+    return String.fromCharCode(s.charCodeAt(0) - 0xfee0)
   })
 }
