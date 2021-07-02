87d88e12f5b32bbedcdc5874320a65a010726198
Keitaroh Kobayashi <keita@kbys.me>
2021-07-02 12:54:06 +0900

    test/addresses.csv の更新

--- test/build-test-data.ts
+++ test/build-test-data.ts
@@ -4,25 +4,27 @@ import path from 'path'
 
 console.log('住所,都道府県,市区町村,町丁目,その他')
 
-const data = fs.readFileSync(path.join(path.dirname(__filename), '/list.txt'), {encoding: 'utf-8'}).split(/\n/)
+const data = fs
+  .readFileSync(path.join(path.dirname(__filename), '/list.txt'), {
+    encoding: 'utf-8',
+  })
+  .split(/\n/)
 
-const lines = []
-
-for (let i = 0; i < data.length; i++) {
-  const address = data[i].trim()
-  if (! address) {
-    continue
-  }
-
-  normalize(address).then(result => {
+;(async () => {
+  data.sort()
+  for (let i = 0; i < data.length; i++) {
+    const address = data[i].trim()
+    if (!address) {
+      continue
+    }
+    const result = await normalize(address)
     const line = [
       address,
       result.pref,
       result.city,
       result.town,
-      result.addr
+      result.addr,
     ].join(',')
-
     console.log(line)
-  })
-}
+  }
+})()
