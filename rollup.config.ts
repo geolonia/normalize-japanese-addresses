1eea3482afc80631fa7ef95e7d8ed607e351547e
Keitaroh Kobayashi <keita@kbys.me>
2021-09-15 14:45:43 +0900

    Build separate entrypoint for browser (#120)
    ブラウザ用の読み込むJavaScriptをnode用と分ける

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,22 @@
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
 
-export default {
-  input: 'src/main.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
+export default [
+  {
+    input: 'src/main.ts',
+    output: {
+      dir: 'dist',
+      name: 'normalize',
+      format: 'umd',
+    },
+    plugins: [typescript(), resolve({ browser: true }), commonjs()],
   },
-  plugins: [typescript(), resolve(), commonjs()],
-  external: ['axios'],
-}
+  {
+    input: 'src/main.ts',
+    external: ['@geolonia/japanese-numeral', 'isomorphic-unfetch', 'lru-cache'],
+    output: { file: pkg.main, format: 'cjs' },
+    plugins: [typescript()],
+  },
+]
