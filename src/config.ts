339cf982518b547b741a5dd87972c9602a1564f5
Keitaroh Kobayashi <keita@kbys.me>
2021-10-26 12:24:48 +0900

    RegExpメモリ改善 (#128)
    * getTownRegexes の RegExp をコンパイルせずstringだけキャッシュする
    
    cachedTownRegexes のLRU上限もconfigで設定できるようにしました
    
    * Don't reuse RegExp objects
    
    * Renamed all variables that contain regex patterns to 'pattern' to correctly reflect they contain a regex pattern instead of a regex object

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 export interface Config {
   japaneseAddressesApi: string
+
+  /** 都道府県＋市区町村のデータを何件までキャッシュするか。デフォルト 1,000 */
+  townCacheSize: number
 }
 
 export const currentConfig: Config = {
   japaneseAddressesApi: 'https://geolonia.github.io/japanese-addresses/api/ja',
+  townCacheSize: 1_000,
 }
