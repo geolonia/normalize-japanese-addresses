32f67860e5f5c346d9888b68fcc76a647a5be561
kamataryo <kamataryo@users.noreply.github.com>
2021-12-07 10:39:59 +0900

    Enhance type document

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  plugins: ['eslint-plugin-tsdoc'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
@@ -13,5 +14,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/ban-ts-comment': 0,
     '@typescript-eslint/no-var-requires': 0,
+    'tsdoc/syntax': 'error',
   },
 }
