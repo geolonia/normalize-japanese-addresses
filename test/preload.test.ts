203b34cc7fed8ae2462dfdac903086b7aef716b6
Keitaroh Kobayashi <keita@kbys.me>
2022-01-18 11:36:22 +0900

    Set timeout to 200ms, log preload time

--- test/preload.test.ts
+++ test/preload.test.ts
@@ -4,16 +4,20 @@ import { cachedTownRegexes } from '../src/lib/cacheRegexes'
 import { performance } from 'perf_hooks'
 
 jest.setTimeout(3 * 60 * 1000)
-beforeAll(preload)
+beforeAll(async () => {
+  console.time('preload')
+  await preload()
+  console.timeEnd('preload')
+})
 
 test('should preload cache', () => {
   expect(cachedTownRegexes.length).toBeGreaterThan(1800)
 }, 5000)
 
-test('normalize should take less than 100ms to complete', async () => {
+test('normalize should take less than 200ms to complete', async () => {
   const started = performance.now()
   await normalize('京都府京田辺市同志社山手４丁目１－４３')
   const finished = performance.now()
   console.log(finished - started)
-  expect(finished - started).toBeLessThan(100)
+  expect(finished - started).toBeLessThan(200)
 }, 5000)
