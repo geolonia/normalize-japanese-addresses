896e9e0a64fcd277d9907aa810d105062f32df1c
Kamata, Ryo <ryo@geolonia.com>
2022-01-27 10:41:01 +0900

    全てのエントリポイントでテストする (#149)
    * Add tests for all entry points
    
    Co-authored-by: kamataryo <kamataryo@users.noreply.github.com>

--- test/addresses.test.ts
+++ test/addresses.test.ts
@@ -1,4 +1,5 @@
-import { normalize } from '../src/main-node'
+import { normalize as normalizerForNode } from '../src/main-node'
+import { normalize as normalizerForBrowser } from '../src/main-browser'
 
 import fs from 'fs'
 import path from 'path'
@@ -10,16 +11,28 @@ const lines = fs
   .split(/\n/)
 lines.shift() // 見出し行
 
-lines.forEach((line) => {
-  if (line) {
-    const data = line.trim().split(/,/)
+const cases: [
+  runtime: string,
+  normalizer: typeof normalizerForNode | typeof normalizerForBrowser,
+][] = [
+  ['node', normalizerForNode],
+  ['browser', normalizerForBrowser],
+]
 
-    test(data[0], async () => {
-      const res = await normalize(data[0])
-      expect(res.pref).toStrictEqual(data[1])
-      expect(res.city).toStrictEqual(data[2])
-      expect(res.town).toStrictEqual(data[3])
-      expect(res.addr).toStrictEqual(data[4])
+for (const [runtime, normalize] of cases) {
+  describe(`tests for ${runtime} entry point`, () => {
+    lines.forEach((line) => {
+      if (line) {
+        const data = line.trim().split(/,/)
+
+        test(data[0], async () => {
+          const res = await normalize(data[0])
+          expect(res.pref).toStrictEqual(data[1])
+          expect(res.city).toStrictEqual(data[2])
+          expect(res.town).toStrictEqual(data[3])
+          expect(res.addr).toStrictEqual(data[4])
+        })
+      }
     })
-  }
-})
+  })
+}
