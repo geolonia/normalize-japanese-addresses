ab7632288f36f67233451f2e74ff7fa5aadcdd81
Kamata, Ryo <ryo@geolonia.com>
2023-09-07 09:12:33 +0900

    漢数字の小字でクラッシュする問題に対応 (#221)
    * Add test case with 愛知県豊田市西丹波町三五十 (#215)  and 広島県府中市栗柄町名字八五十2459 (#214)
    
    * issue #215 fix
    
    ---------
    
    Co-authored-by: miyabe <tatsuhiko@miya.be>

--- src/lib/kan2num.ts
+++ src/lib/kan2num.ts
@@ -3,8 +3,12 @@ import { kanji2number, findKanjiNumbers } from '@geolonia/japanese-numeral'
 export const kan2num = (string: string) => {
   const kanjiNumbers = findKanjiNumbers(string)
   for (let i = 0; i < kanjiNumbers.length; i++) {
-    // @ts-ignore
-    string = string.replace(kanjiNumbers[i], kanji2number(kanjiNumbers[i]))
+    try {
+      // @ts-ignore
+      string = string.replace(kanjiNumbers[i], kanji2number(kanjiNumbers[i]));
+    } catch (error) {
+      // ignore
+    }
   }
 
   return string
