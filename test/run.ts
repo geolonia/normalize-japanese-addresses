fd2c37c82d777c8790fc16c06497a8ce5a328977
Keita Kobayashi <keita@kbys.me>
2024-10-23 06:39:16 +0900

    Use built artifacts to test:integration in matrix

--- test/run.ts
+++ test/run.ts
@@ -1,27 +1,28 @@
-import { spec } from 'node:test/reporters'
-import { run } from 'node:test'
-import { finished } from 'node:stream/promises'
-import path from 'node:path'
-import { glob } from 'glob'
+import { spec as specReporter } from 'node:test/reporters';
+import { run } from 'node:test';
+import process from 'node:process';
+import { finished } from 'node:stream/promises';
+import path from 'node:path';
+import { glob } from 'glob';
 
 async function main(base: string, ...testNamePatterns: string[]) {
-  const files = await glob(path.join('test', base, '**', '*.test.ts'))
-
+  const files = await glob(path.join('test', base, '**', '*.test.ts'), {
+    absolute: true,
+  });
   const testStream = run({
     files,
     testNamePatterns,
-    forceExit: true,
-  })
+  });
   testStream.on('test:fail', () => {
     process.exitCode = 1
   });
-  const outStream = testStream.compose(spec).pipe(process.stdout)
-  await finished(outStream)
+  const outStream = testStream.compose(new specReporter()).pipe(process.stdout);
+  await finished(outStream);
 }
 
 main(process.argv[2], ...process.argv.slice(3))
   .then(() => {
-    process.exit(0)
+    process.exit()
   })
   .catch((e) => {
     console.error(e)
