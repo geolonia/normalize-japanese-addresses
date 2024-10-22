bc0b783bc0ab2abd6fabae12f9654c6bacd296e2
Keita Kobayashi <keita@kbys.me>
2024-10-22 21:02:08 +0900

    Run lint

--- test/integration/node.test.ts
+++ test/integration/node.test.ts
@@ -5,21 +5,21 @@ import { promisify } from 'node:util'
 import { exec as execCb } from 'node:child_process'
 import path from 'node:path'
 
-const exec = promisify(execCb);
+const exec = promisify(execCb)
 
 const runTest = (type: 'cjs' | 'esm') => {
   return async () => {
     // the test project is in ./node-${type}
-    const cwd = path.join(import.meta.dirname, `node-${type}`);
-    await exec(`npm install`, { cwd });
-    const res = await exec(`node ./index.js`, { cwd });
+    const cwd = path.join(import.meta.dirname, `node-${type}`)
+    await exec(`npm install`, { cwd })
+    const res = await exec(`node ./index.js`, { cwd })
 
-    assert.strictEqual(res.stdout.trim(), 'OK');
-    assert.strictEqual(res.stderr, '');
+    assert.strictEqual(res.stdout.trim(), 'OK')
+    assert.strictEqual(res.stderr, '')
   }
 }
 
 describe(`node`, () => {
-  test(`cjs`, runTest('cjs'));
-  test(`esm`, runTest('esm'));
-});
+  test(`cjs`, runTest('cjs'))
+  test(`esm`, runTest('esm'))
+})
--- test/integration/webpack.test.ts
+++ test/integration/webpack.test.ts
@@ -5,16 +5,16 @@ import { promisify } from 'node:util'
 import { exec as execCb } from 'node:child_process'
 import path from 'node:path'
 
-const exec = promisify(execCb);
+const exec = promisify(execCb)
 
 describe(`webpack with TS`, { timeout: 60_000 }, () => {
   test(`build & run`, async () => {
-    const cwd = path.join(import.meta.dirname, `webpack-ts`);
-    await exec(`npm install`, { cwd });
-    await exec(`npm run build`, { cwd });
-    const res = await exec(`node ./dist/bundle.js`, { cwd });
+    const cwd = path.join(import.meta.dirname, `webpack-ts`)
+    await exec(`npm install`, { cwd })
+    await exec(`npm run build`, { cwd })
+    const res = await exec(`node ./dist/bundle.js`, { cwd })
 
-    assert.strictEqual(res.stdout.trim(), 'OK');
-    assert.strictEqual(res.stderr, '');
-  });
-});
+    assert.strictEqual(res.stdout.trim(), 'OK')
+    assert.strictEqual(res.stderr, '')
+  })
+})
