d919d005f478333523c88b6a646c1493ebab7691
Keita Kobayashi <keita@kbys.me>
2024-12-23 17:45:20 +0900

    NormalizeResult 型をexportする

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as Normalize from './normalize'
 
+export type { NormalizeResult } from './types'
+
 export const version = Normalize.version
 export const config = Normalize.config
 export const normalize = Normalize.normalize
